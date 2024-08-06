330f53b41816e4d7b164b29fa8120d8b9d130ece
unknown <manoranjanjenamitsy17@gmail.com>
2024-08-07 00:36:01 +0530

    Done some changes in readme file

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ Currently, two official plugins are available:
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
 
 ->We are using lucide-react for icons
+->get navitems from constant folder
+
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import logo from "../assets/logo.png"
+import { navItems } from "../constants/index"
 
 const Navbar = () => {
+    console.log(navItems); 
   return (
     <nav className="sticky top-0.z-50.py-3.backdrop-blur-lg.border-b.border-neutral-700/80">
         <div className="container px-4 mx-auto relative text-sm">
@@ -9,6 +11,15 @@ const Navbar = () => {
                     <img className="h-10 w-10 mr-2" src={logo} alt="logo" />
                     <span className="text-xl tracking-tight">VirtualR</span>
                 </div>
+                <ul className="hidden lg:flex ml-14 space-x-12">
+                    {navItems.map((item,index)=>(
+                        <li key={index}>
+                            <a href={item.href}>{item.label}</a>
+                        </li>
+
+                    ))}
+
+                </ul>
 
             </div>
 
